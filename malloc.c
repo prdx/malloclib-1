e10372422546d18105f173a747cb3f872b2a2ea0
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-02-04 10:50:50 -0500

    Add initi

--- malloc.c
+++ malloc.c
@@ -1,10 +1,16 @@
 #include <unistd.h>     /* for sbrk, sysconf */
 #include "mallutl.h"    /* for data structure */
+#include <math.h>
 
 /* The memory request must be always multiple of PAGE_SIZE */
-#ifndef malloc_getpagesize
-#define malloc_getpagesize   sysconf(_SC_PAGE_SIZE)
-#endif
+#define HEAP_PAGE_SIZE   sysconf(_SC_PAGE_SIZE)
+#define BASE 2
+#define DATA_OFFSET     offsetof(header_t, block_data)
+#define SIZE_TO_ORDER(size) ( ceil( (log(size)/log(BASE)) ) )
+#define MAX_ORDER 12 /* 2^12 = 4096 */
+
+void *find_suitable_space(size_t);
+int init(size_t);
 
 /*
  * This function will get the initial heap memory to allocate (or probably other
@@ -13,22 +19,74 @@
  *
 */
 
-Node *head = NULL;
+arena_t* arenas = NULL;
+int arena_index = 0;
 
 /*
   Buddy allocation malloc:
 */
-void *malloc(size_t size) {
+void *malloc(size_t block) {
+  void *addr = NULL;
 
-  /* 1. Initialize: when the list is empty, we create a new head and then get 
-     memory from the OS. We should probably also check what is the size. 
-     Since, we can directly ask the OS the (size / PAGE_SIZE + 1) * PAGE_SIZE 
-  */
-  size_t size_allocated = (size / malloc_getpagesize + 1) * malloc_getpagesize;
-  if(head == NULL) {
-    head = sbrk(size_allocated);
-    head->size = size_allocated;
-    head->next = NULL;
+  // if no empty block found, create new arena
+  if(init(block) == -1) {
+    MALLOC_FAILURE_ACTION;
+    return NULL;
+  }
+}
+
+int init(size_t block) {
+  void *addr;
+  int count = 0;
+
+  unsigned data_size = block / HEAP_PAGE_SIZE + 1;
+  size_t allocated_memory = sizeof(arena_t) +  /* Arena header */
+    sizeof(header_t) * data_size * 512 +       /* Block headers */
+    data_size * HEAP_PAGE_SIZE;                /* Data */
+
+  if((addr = sbrk(allocated_memory)) == (void*) -1) {
+    return -1;
+  }
+
+  if(arenas == NULL) {
+    arenas = (arena_t*) addr;
+    header_t* header = (header_t*)(addr + sizeof(arena_t));
+    void* data = addr + sizeof(header_t) * data_size * 512;
+    arenas->base_header = header;
+    arenas->next = NULL;
+    arenas->size = allocated_memory;
+    header->address = data;
+    header->next = NULL;
+    header->is_free = 1;
+    header->size = SIZE_TO_ORDER(block);
+    count = 1;
+  }
+  else {
+    count = arena_index;
+    arena_t *arena = arenas;
+    while(arena->next != NULL) {
+      arena = arena->next;  /* get the tail */
+    }
+    arena->next = addr;     /* add new node */ 
+    arena = arena->next;    /* move to the new node */
+    header_t* header = (header_t*)(addr + sizeof(arena_t));
+    void* data = addr + sizeof(header_t) * data_size * 512;
+    arena->base_header =  header;
+    arena->next = NULL;
+    arena->size = allocated_memory;
+    header->address = data;
+    header->next = NULL;
+    header->is_free = 1;
+    header->size = SIZE_TO_ORDER(block);
+    count++;
+  }
+  return count;
+}
+
+void* find_suitable_space(size_t block) {
+  arena_t *arena = arenas;
+  while(arena->next != NULL) {
+    arena = arena->next;
   }
 }
 
