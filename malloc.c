37f77eb349f9f0b9aaddefb8b4cf60f290368152
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-02-05 09:53:40 -0500

    Fix bug

--- malloc.c
+++ malloc.c
@@ -1,19 +1,19 @@
-#include <unistd.h>     /* for sbrk, sysconf */
-#include "mallutl.h"    /* for data structure */
+#include <unistd.h>  /* for sbrk, sysconf */
+#include "mallutl.h" /* for data structure */
 #include <math.h>
 
 /* The memory request must be always multiple of PAGE_SIZE */
-#define HEAP_PAGE_SIZE   sysconf(_SC_PAGE_SIZE)
+#define HEAP_PAGE_SIZE sysconf(_SC_PAGE_SIZE)
 #define BASE 2
-#define DATA_OFFSET     offsetof(header_t, block_data)
-#define SIZE_TO_ORDER(size) ( ceil( (log(size)/log(BASE)) ) )
+#define DATA_OFFSET offsetof(header_t, block_data)
+#define SIZE_TO_ORDER(size) (ceil((log(size) / log(BASE))))
 #define MAX_ORDER 12 /* 2^12 = 4096 */
 
 void *find_suitable_space(size_t);
 int init(size_t);
-int is_need_split(header_t*, size_t);
-void split(header_t*, size_t);
-void add_new_header(header_t*);
+int is_need_split(header_t *, size_t);
+void split(header_t *, size_t);
+void add_new_header(header_t *);
 
 /*
  * This function will get the initial heap memory to allocate (or probably other
@@ -22,8 +22,8 @@ void add_new_header(header_t*);
  *
 */
 
-arena_t* arenas = NULL;
-arena_t* current_arena = NULL;
+arena_t *arenas = NULL;
+arena_t *current_arena = NULL;
 int arena_index = 0;
 
 /*
@@ -33,44 +33,42 @@ void *my_malloc(size_t block) {
   header_t *addr = NULL;
 
   // if no empty block found, create new arena
-  if((addr = find_suitable_space(block)) == (void*) -1) {
-    if((arena_index = init(block)) == -1) {
+  if ((addr = find_suitable_space(block)) == (void *)-1) {
+    if ((arena_index = init(block)) == -1) {
       MALLOC_FAILURE_ACTION;
       return NULL;
     }
     addr = arenas->base_header;
   }
-  
+
   // found empty block, either split or just fill it
-  if(is_need_split(addr, block)) {
+  if (is_need_split(addr, block)) {
     split(addr, block);
   }
 
   addr->is_free = 0;
   return addr->address;
-
 }
-
-
 
 int init(size_t block) {
   void *addr;
   int count = 0;
 
   unsigned data_size = block / HEAP_PAGE_SIZE + 1;
-  size_t allocated_memory = sizeof(arena_t) +  /* Arena header */
-    sizeof(header_t) * data_size * 512 +       /* Block headers */
-    data_size * HEAP_PAGE_SIZE;                /* Data */
+  size_t allocated_memory =
+      sizeof(arena_t) +                    /* Arena header */
+      sizeof(header_t) * data_size * 512 + /* Block headers */
+      data_size * HEAP_PAGE_SIZE;          /* Data */
 
-  if((addr = sbrk(allocated_memory)) == (void*) -1) {
+  if ((addr = sbrk(allocated_memory)) == (void *)-1) {
     return -1;
   }
 
-  if(arenas == NULL) {
+  if (arenas == NULL) {
     /* Assign value to the node */
-    arenas = (arena_t*) addr;
-    header_t* header = (header_t*)(addr + sizeof(arena_t));
-    void* data = addr + sizeof(header_t) * data_size * 512;
+    arenas = (arena_t *)addr;
+    header_t *header = (header_t *)(addr + sizeof(arena_t));
+    void *data = addr + sizeof(header_t) * data_size * 512;
     arenas->base_header = header;
     arenas->next = NULL;
     arenas->size = allocated_memory;
@@ -81,20 +79,19 @@ int init(size_t block) {
     header->is_free = 1;
     header->size = SIZE_TO_ORDER(data_size * HEAP_PAGE_SIZE);
     count = 1;
-  }
-  else {
+  } else {
     count = arena_index;
     arena_t *arena = arenas;
-    while(arena->next != NULL) {
-      arena = arena->next;  /* get the tail */
+    while (arena->next != NULL) {
+      arena = arena->next; /* get the tail */
     }
-    arena->next = addr;     /* add new node */ 
-    arena = arena->next;    /* move to the new node */
+    arena->next = addr;  /* add new node */
+    arena = arena->next; /* move to the new node */
     current_arena = arena;
     /* assign value to the node */
-    header_t* header = (header_t*)(addr + sizeof(arena_t));
-    void* data = addr + sizeof(header_t) * data_size * 512;
-    arena->base_header =  header;
+    header_t *header = (header_t *)(addr + sizeof(arena_t));
+    void *data = addr + sizeof(header_t) * data_size * 512;
+    arena->base_header = header;
     arena->next = NULL;
     arena->size = allocated_memory;
     arena->header_index = 1;
@@ -107,38 +104,29 @@ int init(size_t block) {
   return count;
 }
 
-void* find_suitable_space(size_t block) {
-  if(arenas == NULL) return (void*) -1;
+void *find_suitable_space(size_t block) {
+  if (arenas == NULL)
+    return (void *)-1;
   arena_t *arena = arenas;
-  /*FIXME*/
-  header_t *need_split = arena->base_header;
-  while(arena != NULL) {
+
+  while (arena != NULL) {
     header_t *header = arena->base_header;
-    while(header != NULL) {
-      printf("Is free: %d\n", header->is_free);
-      if(header->is_free == 1 && pow(2, header->size) / 2 < block) {
-        current_arena = arena;
-        return header;
-      }
-      else if(header->is_free == 1 && pow(2, header->size) / 2 >= block) {
-        if(need_split->size > header->size) {
-          /* If we find the smaller order after the bigger order, switch */
-          need_split = header;  
+    while (header != NULL) {
+      if(header->is_free == 1) {
+        if (pow(2, header->size) >= block) {
+          current_arena = arena;
+          return header;
         }
       }
       header = header->next;
     }
-    if(need_split->is_free == 1 && pow(2, need_split->size) / 2 >= block) {
-      current_arena = arena;
-      return need_split;
-    }
-    arena = arena->next;  /* get the tail */
+    arena = arena->next; /* get the tail */
   }
-  return (void*) -1;
+  return (void *)-1;
 }
 
 int is_need_split(header_t *header, size_t block) {
-  if(header->is_free == 1 && pow(2, header->size) / 2 < block) {
+  if (header->is_free == 1 && pow(2, header->size) / 2 < block) {
     return 0;
   }
   return 1;
@@ -152,20 +140,20 @@ void split(header_t *header, size_t block) {
   /*printf("Next: %p\n", header->next);*/
   /*printf("------------------------------------\n");*/
 
-  if((pow(2, header->size) / 2) <= block && pow(2, header->size) > block) {
+  if ((pow(2, header->size) / 2) <= block && pow(2, header->size) > block) {
     return;
   }
 
-  // Adding new node, is to put the the new node in the 
+  // Adding new node, is to put the the new node in the
   // head address + sizeof(header_t)
   add_new_header(header);
   split(header, block);
 }
 
-
 void add_new_header(header_t *header) {
   header_t *current = header;
-  header_t *new = current_arena->base_header + sizeof(header) * current_arena->header_index;
+  header_t *new =
+      current_arena->base_header + sizeof(header) * current_arena->header_index;
   new->is_free = 1;
   new->size = header->size - 1;
   new->next = NULL;
@@ -192,9 +180,9 @@ void add_new_header(header_t *header) {
 void debug_info() {
   arena_t *arena = arenas;
   header_t *header = NULL;
-  while(arena != NULL) {
+  while (arena != NULL) {
     header = arena->base_header;
-    while(1) {
+    while (1) {
       printf("----------------NODE---------------\n");
       printf("Location: %p\n", header);
       printf("Data address: %p\n", header->address);
@@ -202,7 +190,7 @@ void debug_info() {
       printf("Free status: %d\n", header->is_free);
       printf("Next: %p\n", header->next);
       printf("------------------------------------\n");
-      if(header->next == NULL) {
+      if (header->next == NULL) {
         break;
       }
       header = header->next;
@@ -211,21 +199,26 @@ void debug_info() {
   }
 }
 
-int main()
-{
+int main() {
   char *a;
   char *b;
+  char *c;
   a = my_malloc(1000);
-  printf("Malloc address: %p\n", (void*)a);
+  printf("Malloc address: %p\n", (void *)a);
   printf("Trying to store data now\n");
   a = "hello world";
   printf("Result: %s\n", a);
-  debug_info();
   b = my_malloc(10);
-  printf("Malloc address: %p\n", (void*)b);
+  printf("Malloc address: %p\n", (void *)b);
   printf("Trying to store data now\n");
   b = "hello man";
   printf("Result: %s\n", b);
+  c = my_malloc(10);
+  printf("Malloc address: %p\n", (void *)c);
+  printf("Trying to store data now\n");
+  c = "yolo man";
+  printf("Result: %s\n", c);
+  debug_info();
   return 0;
 }
 
