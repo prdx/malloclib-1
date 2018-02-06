44ef7ac5d7841d2b50c99178d6c9fadf7220cabf
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-02-06 09:32:07 -0500

    Add reallocarray

--- malloc.c
+++ malloc.c
@@ -296,3 +296,20 @@ void *realloc(void *ptr, size_t size) {
 
   return addr;
 }
+
+/*------------REALLOCARRAY---------------
+ Equivalent to realloc(ptr, nmemb * size), except it handles int overflow
+ Source: 
+ https://www.gnu.org/software/libc/manual/html_node/Changing-Block-Size.html
+*/
+void *rallocarray(void *ptr, size_t nmemb, size_t size) {
+  size_t total = nmemb * size;
+  // handle the integer overflow
+  if(nmemb != 0 && total / nmemb != size) {
+    errno = ENOMEM;
+    return NULL;
+  }
+
+  void *addr = realloc(ptr, total);
+  return addr;
+}
